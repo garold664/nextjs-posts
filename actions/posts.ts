4dda13130405b22ecc46c9c5c4e9ab769ce67d85
garold664 <garold664@gmail.com>
2024-06-03 13:42:59 +0300

    moved createPost server action into separate file dedicated for server actions

---
+++ actions/posts.ts
@@ -0,0 +1,37 @@
+'use server';
+import { redirect } from 'next/navigation';
+import { storePost } from '@/lib/posts';
+export async function createPost(
+  prevState: { errors: string[] },
+  formData: FormData
+) {
+  const title = formData.get('title') as string | null;
+  const image = formData.get('image') as File | null;
+  const content = formData.get('content') as string | null;
+
+  let errors: string[] = [];
+
+  if (!title || title.trim().length === 0) {
+    errors.push('Title is required');
+  }
+  if (!title || title.trim().length === 0) {
+    errors.push('Title is required');
+  }
+
+  if (!image || image.size === 0) {
+    errors.push('Image is required');
+  }
+
+  if (errors.length > 0) {
+    return { errors };
+  }
+
+  await storePost({
+    imageUrl: '',
+    title,
+    content,
+    userId: 1,
+  });
+
+  redirect('/feed');
+}
